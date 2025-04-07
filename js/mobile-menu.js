7396cae9dea8d79d288104934fbcc0d6d1663ce7
L. F. <101153424+LFreitas88@users.noreply.github.com>
2025-04-07 13:45:27 -0400

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,9 +1,12 @@
-// /js/mobile-menu.js
 document.addEventListener('DOMContentLoaded', () => {
   const menuBtn = document.getElementById('mobile-menu-button');
   const mobileMenu = document.getElementById('mobile-menu');
 
-  menuBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-  });
+  if (menuBtn && mobileMenu) {
+    menuBtn.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+    });
+  } else {
+    console.warn('Mobile menu or button not found.');
+  }
 });
